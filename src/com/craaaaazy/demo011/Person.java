b0461d1b2aeaeeba0179703a9392f6c5eaa09788
d.zboy <d.zboy@qq.com>
2018-09-22 01:11:59 +0800

    8节包名内容

---
+++ src/com/craaaaazy/demo011/Person.java
@@ -0,0 +1,9 @@
+    package com.craaaaazy.demo011;
+
+    public class Person {
+        public String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+    }
---
+++ src/com/craaaaazy/demo012/Demo.java
@@ -0,0 +1,11 @@
+package com.craaaaazy.demo012;
+
+import com.craaaaazy.demo011.Person;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Person person = new Person("July");
+        System.out.println(person.name);
+    }
+}
